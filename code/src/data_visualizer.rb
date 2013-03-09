82997bfadd620d3dca79ad4bd486dcb0913a6d06
Thomas Dimson <tdimson@gmail.com>
2013-03-08 22:31:26 -0800

    Add data visualizer

---
+++ code/src/data_visualizer.rb
@@ -0,0 +1,59 @@
+require 'erb'
+require 'java'
+
+java_import "edu.stanford.cs224u.disentanglement.structures.DataSets"
+MessageTree = Java::edu.stanford.cs224u.disentanglement.structures.MessageTree
+
+
+class RenderTree
+  attr_accessor :id
+  attr_accessor :gold_tree
+  attr_accessor :guess_tree
+end
+
+
+class Visualizer
+  attr_reader :master_list
+  attr_reader :node, :alt
+
+  def initialize(output_file, gold_trees, guess_trees)
+    @output_file = output_file
+    @master_list = []
+    i = 0
+    gold_trees.zip(guess_trees) do |gold_tree, guess_tree|
+      rt = RenderTree.new
+      rt.id = i
+      rt.gold_tree = gold_tree
+      rt.guess_tree = guess_tree
+      @master_list << rt
+      i += 1
+    end
+  end
+
+  def render_tree_node(node, alt=false)
+    old_node = node
+    old_alt = alt
+    @node = node
+    @alt = alt
+    ret = ERB.new(open("code/src/data_visualizer_tree.erb.html").read).result(get_binding)
+    @node = old_node
+    @alt = old_alt
+    ret
+  end
+
+  def render_all
+    res = ERB.new(open("code/src/data_visualizer.erb.html").read).result(get_binding)
+    open(@output_file, 'w') { |f| f.write(res)  }
+    puts "Wrote to #{@output_file}"
+  end
+
+  def get_binding
+    binding()
+  end
+
+end
+
+
+if __FILE__ == $0
+  Visualizer.new("visuals/last_run.html", DataSets::ASK_REDDIT_DEV.read, DataSets::ASK_REDDIT_DEV.read).render_all
+end
--- code/src/java/edu/stanford/cs224u/disentanglement/DisentanglementPipeline.java
+++ code/src/java/edu/stanford/cs224u/disentanglement/DisentanglementPipeline.java
@@ -1,7 +1,13 @@
 package edu.stanford.cs224u.disentanglement;
 
+import javax.script.ScriptContext;
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+
 import com.google.common.base.Preconditions;
 import com.google.common.collect.ImmutableList;
+import com.google.common.collect.Lists;
 import edu.stanford.cs224u.disentanglement.disentanglers.Disentangler;
 import edu.stanford.cs224u.disentanglement.evaluation.AverageTreeF1Evaluator;
 import edu.stanford.cs224u.disentanglement.evaluation.Evaluator;
@@ -18,6 +24,7 @@ public class DisentanglementPipeline {
     private DataSets testData;
     private Disentangler disentangler;
     private List<Evaluator> evaluators;
+    private String visualizationFile;
 
     public DisentanglementPipeline() {
         evaluators = ImmutableList.of(
@@ -25,6 +32,7 @@ public class DisentanglementPipeline {
                 new PairwiseF1Evaluator(),
                 new AverageTreeF1Evaluator()
         );
+        visualizationFile = "visuals/last_run.html";
     }
 
     public DisentanglementPipeline withEvaluators(Iterable<Evaluator> evaluators) {
@@ -47,11 +55,18 @@ public class DisentanglementPipeline {
         return this;
     }
 
+    public DisentanglementPipeline withVisualizationFile(String visualizationFile) {
+        this.visualizationFile = visualizationFile;
+        return this;
+    }
+
     public void run() {
         Preconditions.checkNotNull(trainData, "Must specify training data for pipeline!");
         Preconditions.checkNotNull(testData, "Must specify testing data for pipeline!");
         Preconditions.checkNotNull(disentangler, "Must specify a disentangler for pipeline!");
 
+        List<MessageTree> goldTrees = Lists.newArrayList();
+        List<MessageTree> guessTrees = Lists.newArrayList();
 
         Benchmarker.push("Training");
         disentangler.train(trainData.read());
@@ -63,6 +78,14 @@ public class DisentanglementPipeline {
             for(Evaluator evaluator : evaluators) {
                 evaluator.addPrediction(gold, guess);
             }
+            goldTrees.add(gold);
+            guessTrees.add(guess);
+        }
+
+        if(visualizationFile != null) {
+            Benchmarker.push("Write visualization");
+            writeVisualization(goldTrees, guessTrees);
+            Benchmarker.pop();
         }
 
         System.out.println("Evaluations: ");
@@ -70,5 +93,21 @@ public class DisentanglementPipeline {
             System.out.println("\t" + e.getEvaluation());
         }
         Benchmarker.pop();
+    }
+
+    private void writeVisualization(List<MessageTree> goldTrees, List<MessageTree> guessTrees) {
+        ScriptEngineManager m = new ScriptEngineManager();
+        ScriptEngine rubyEngine = m.getEngineByName("jruby");
+        ScriptContext context = rubyEngine.getContext();
+
+        context.setAttribute("goldTrees", goldTrees, ScriptContext.ENGINE_SCOPE);
+        context.setAttribute("guessTrees", guessTrees, ScriptContext.ENGINE_SCOPE);
+
+        try {
+            rubyEngine.eval("require \"./code/src/data_visualizer\";" +
+                    "Visualizer.new(\"" + visualizationFile +"\", $goldTrees, $guessTrees).render_all", context);
+        } catch (ScriptException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
